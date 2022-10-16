9c69e762cd0de391d27b2504dac9df6f6de63972
Markfryazino <Rofinmp@gmail.com>
2022-10-16 19:06:59 +0300

    add attention shift

--- src/data.py
+++ src/data.py
@@ -44,16 +44,43 @@ class CoilDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         graph = self.coil_graphs[idx]
         node_features = torch.vstack([torch.tensor(graph.nodes[i]["hist"]) for i in graph.nodes])
-        return {"node_features": node_features, "target": self.targets[idx]}
+        node_centralities = [graph.degree[i] for i in graph.nodes]
+
+        shortest_paths_dict = dict(nx.all_pairs_shortest_path(graph))
+        paths = [[shortest_paths_dict[v][u] for u in graph.nodes] for v in graph.nodes]
+        shortest_paths_lengths_dict = dict(nx.all_pairs_shortest_path_length(graph))
+        path_lengths = [[shortest_paths_lengths_dict[v][u] for u in graph.nodes] for v in graph.nodes]
+
+        for u in range(len(graph.nodes)):
+            for v in range(len(graph.nodes)):
+                p = paths[u][v]
+                features = torch.zeros(len(p) - 1, 1)
+                for i in range(len(p) - 1):
+                    features[i] = graph.get_edge_data(p[i], p[i+1])["length"]
+                paths[u][v] = features
+
+        return {"node_features": node_features, "target": self.targets[idx], 
+                "node_centralities": node_centralities, "paths": paths, "path_lengths": path_lengths}
 
 
 def collate_graphs(data):
     max_nodes = max([x["node_features"].shape[0] for x in data])
     mask = torch.zeros(len(data), max_nodes)
     node_features = torch.zeros(len(data), max_nodes, data[0]["node_features"].shape[1])
+    node_centralities = torch.zeros(len(data), max_nodes)
 
     for i, x in enumerate(data):
         mask[i, :x["node_features"].shape[0]] = 1
         node_features[i, :x["node_features"].shape[0]] = x["node_features"]
+        node_centralities[i, :x["node_features"].shape[0]] = torch.tensor(x["node_centralities"])
+
+    target = torch.tensor([x["target"] for x in data])
+    paths = [x["paths"] for x in data]
+
+    path_lengths = torch.zeros((len(data), max_nodes, max_nodes))
+
+    for i, x in enumerate(data):
+        path_lengths[i, :x["node_features"].shape[0], :x["node_features"].shape[0]] = torch.tensor(x["path_lengths"])
     
-    return {"node_features": node_features, "mask": mask}
+    return {"node_features": node_features, "mask": mask, "target": target,
+            "node_centralities": node_centralities, "paths": paths, "path_lengths": path_lengths}
--- src/model.py
+++ src/model.py
@@ -62,11 +62,27 @@ class SelfAttentionWithShift(nn.Module):
 
 
 class Shifter(nn.Module):
-    def __init__(self):
+    active = False
+
+    def __init__(self, hidden_dim=64, num_heads=8):
         super().__init__()
 
-    def forward(self, paths):
-        return None
+        self.path_length_embedding = nn.Embedding(512, 1)
+        self.path_idx_embedding = nn.Embedding(512, hidden_dim)
+        self.num_heads = num_heads
+
+    def forward(self, X, mask, paths, path_lengths):
+        if not Shifter.active:
+            return None
+
+        distance_mat = torch.zeros((path_lengths.size(0), path_lengths.size(1) + 1, 
+                                    path_lengths.size(2) + 1)).to(X.device)
+        distance_mat[:, 1:, 1:] = path_lengths
+        distance_mat[:, 0, :] = 1
+        distance_mat[:, :, 0] = 1
+
+        result = self.path_length_embedding(distance_mat.long()).squeeze(-1)
+        return result.repeat_interleave(self.num_heads, 0)
 
 
 class EncoderBlock(nn.Module):
@@ -88,8 +104,8 @@ class EncoderBlock(nn.Module):
 
         self.shifter = Shifter()
 
-    def forward(self, X, mask):
-        shift = self.shifter(X)
+    def forward(self, X, mask, paths, path_lengths):
+        shift = self.shifter(X, mask, paths, path_lengths)
 
         X_normed = self.layer_norm_1(X)
         X_attn = self.attention(q=X_normed, k=X_normed, v=X_normed, mask=mask, shift=shift) + X
@@ -99,7 +115,8 @@ class EncoderBlock(nn.Module):
 
 
 class Dvoryaninormer(nn.Module):
-    def __init__(self, node_dim, edge_dim, num_classes, embedding_dim, n_encoders, **encoder_kwargs):
+    def __init__(self, node_dim, edge_dim, num_classes, embedding_dim, n_encoders, 
+                add_centrality_embedding=False, add_path_length_embedding=False, **encoder_kwargs):
         super(Dvoryaninormer, self).__init__()
         
         self.encoders = nn.ModuleList([EncoderBlock(**encoder_kwargs) for i in range(n_encoders)])
@@ -108,17 +125,33 @@ class Dvoryaninormer(nn.Module):
         self.vnode_embedding = nn.Parameter(torch.randn(embedding_dim))
         self.projection = nn.Linear(embedding_dim, num_classes)
 
+        self.add_centrality_embedding = add_centrality_embedding
+        self.centrality_embedding = nn.Embedding(512, embedding_dim)
+
+        self.add_path_length_embedding = add_path_length_embedding
+        Shifter.active = add_path_length_embedding
+
+        if add_centrality_embedding:
+            print("Adding centrality embedding")
+
+        if add_path_length_embedding:
+            print("Adding path length embedding")
+
     def _add_vnode(self, node_features, mask):
         batch_size, n_nodes, embedding_dim = node_features.size()
         vnode_embedding = self.vnode_embedding.repeat(batch_size, 1)
         return torch.cat([vnode_embedding.unsqueeze(1), node_features], dim=1), \
                torch.cat([torch.ones(batch_size, 1).to(mask.device), mask], dim=1)
 
-    def forward(self, node_features, mask, node_centralities=None, edge_features=None, paths=None):
+    def forward(self, node_features, mask, node_centralities=None, paths=None, path_lengths=None):
         node_features = self.node_embedder(node_features)
+
+        if node_centralities is not None and self.add_centrality_embedding:
+            node_features += self.centrality_embedding(node_centralities)
+
         node_features, mask = self._add_vnode(node_features, mask)
 
         for encoder in self.encoders:
-            node_features = encoder(node_features, mask)
+            node_features = encoder(node_features, mask, paths, path_lengths)
 
         return self.projection(node_features[:, 0, :])
